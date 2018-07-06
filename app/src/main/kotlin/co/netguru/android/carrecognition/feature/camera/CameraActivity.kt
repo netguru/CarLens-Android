381ab8fab659a7323d21af0ffc5bdd0bb03bc802
pbochenski <pawel.bochenski@gmail.com>
2018-07-06 08:51:38 +0200

    added bottom sheet for details (no details yet) (#7)
    * added bottom sheet for details (no details yet)
    
    also removed arFragment in favor of arSceneView because bottom sheet didn't
    play well with fragment
    
    * review fixes
    
    (cherry picked from commit 0e39e00)

--- app/src/main/kotlin/co/netguru/android/carrecognition/feature/camera/CameraActivity.kt
+++ app/src/main/kotlin/co/netguru/android/carrecognition/feature/camera/CameraActivity.kt
@@ -1,24 +1,32 @@
 package co.netguru.android.carrecognition.feature.camera
 
 import android.animation.ValueAnimator
+import android.graphics.drawable.GradientDrawable
 import android.media.Image
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
 import android.util.DisplayMetrics
+import android.view.View
 import co.netguru.android.carrecognition.R
+import co.netguru.android.carrecognition.data.ar.ArActivityUtils
+import co.netguru.android.carrecognition.data.recognizer.Car
 import co.netguru.android.carrecognition.feature.cars.CarListActivity
 import com.google.ar.core.HitResult
 import com.hannesdorfmann.mosby3.mvp.MvpActivity
 import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.camera_view_activity.*
+import kotlinx.android.synthetic.main.camera_activity_bottom_sheet.*
+import kotlinx.android.synthetic.main.camera_activity_container.*
+import kotlinx.android.synthetic.main.camera_activity_content.*
 import javax.inject.Inject
 
 
-abstract class CameraActivity : MvpActivity<CameraContract.View, CameraContract.Presenter>(), CameraContract.View {
+class CameraActivity : MvpActivity<CameraContract.View, CameraContract.Presenter>(), CameraContract.View {
 
     @Inject
     lateinit var cameraPresenter: CameraPresenter
+    private val bottomSheetBehavior by lazy { BottomSheetBehavior.from(bottom_sheet) }
 
-//    private val arFragment by lazy { supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment }
+    private var installRequested = false
     private var recognitionIndicatorAnimator: ValueAnimator? = null
 
     private val cameraWidth by lazy {
@@ -33,31 +41,35 @@ abstract class CameraActivity : MvpActivity<CameraContract.View, CameraContract.
         displayMetrics.heightPixels
     }
 
+    private val cornerRadiusInPixels by lazy {
+        resources.getDimensionPixelSize(R.dimen.corner_radius)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.camera_view_activity)
+        setContentView(R.layout.camera_activity_container)
         setPresenter(createPresenter())
 
-        setupArFragment()
+//        arSceneView.planeRenderer.isEnabled = false
 
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         carListButton.setOnClickListener { showCarList() }
 
-//        makePhotoButton.setOnClickListener {
-//            val frame = arFragment.arSceneView.arFrame ?: return@setOnClickListener
-//            val hitPoint = frame.hitTest(cameraWidth / 2f, cameraHeight / 2f).firstOrNull()
-//
-//            presenter.processHitResult(hitPoint)
-//        }
-//
-//        arFragment.arSceneView.scene.setOnUpdateListener {
-//            arFragment.onUpdate(it)
-//            val frame = arFragment.arSceneView.arFrame ?: return@setOnUpdateListener
-//            if (frame.camera.trackingState != TrackingState.TRACKING) {
-//                return@setOnUpdateListener
-//            }
-//            presenter.frameUpdated()
-//        }
+        bottomSheetBehavior.setBottomSheetCallback(object :
+                BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                animateBottomSheet(bottomSheet, slideOffset)
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> presenter.bottomSheetHidden()
+                }
+            }
+        })
+
+//        ArActivityUtils.requestCameraPermission(this, RC_PERMISSIONS)
     }
 
     private fun showCarList() {
@@ -66,15 +78,20 @@ abstract class CameraActivity : MvpActivity<CameraContract.View, CameraContract.
         CarListActivity.startActivityWithCircleAnimation(this, startX, startY)
     }
 
-    private fun setupArFragment() {
-//        arFragment.planeDiscoveryController.hide()
-//        arFragment.planeDiscoveryController.setInstructionView(null)
-//        arFragment.arSceneView.planeRenderer.isEnabled = false
+    private fun animateBottomSheet(bottomSheet: View, slideOffset: Float) {
+        val shape = bottomSheet.background.current as GradientDrawable
+
+        if (slideOffset > 0) {
+            val radius = (1 - slideOffset) * cornerRadiusInPixels
+            shape.cornerRadius = radius
+        }
     }
 
     override fun onResume() {
         super.onResume()
         presenter.attachView(this)
+//        installRequested = ArActivityUtils.initARView(arSceneView, this, installRequested)
+        frameStreamEnabled(true)
     }
 
     override fun onPause() {
@@ -82,33 +99,90 @@ abstract class CameraActivity : MvpActivity<CameraContract.View, CameraContract.
         presenter.detachView()
         recognitionIndicatorAnimator?.cancel()
         recognitionIndicatorAnimator = null
+
+//        arSceneView.pause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         presenter.destroy()
+//        arSceneView.destroy()
     }
 
     override fun createPresenter(): CameraContract.Presenter {
         return cameraPresenter
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, results: IntArray
+    ) {
+        ArActivityUtils.processPermissionResult(this)
+    }
+
     override fun createAnchor(hitPoint: HitResult, text: String) {
 //        val anchor =
-//            AnchorNode(arFragment.arSceneView.session.createAnchor(hitPoint.hitPose))
-//        anchor.setParent(arFragment.arSceneView.scene)
+//            AnchorNode(arSceneView.session.createAnchor(hitPoint.hitPose))
+//        anchor.setParent(arSceneView.scene)
 //        anchor.addChild(StickerNode(text, this))
     }
 
     override fun acquireFrame(): Image? = null
-//        val frame = arFragment.arSceneView.arFrame ?: return null
+//        val frame = arSceneView.arFrame ?: return null
 //        if (frame.camera.trackingState != TrackingState.TRACKING) {
 //            return null
 //        }
 //        return try {
-//            arFragment.arSceneView.arFrame.acquireCameraImage()
+//            arSceneView.arFrame.acquireCameraImage()
 //        } catch (e: NotYetAvailableException) {
 //            null
 //        }
 //    }
+
+    override fun updateViewFinder(viewfinderSize: Float) {
+//        recognitionIndicatorAnimator =
+//                ValueAnimator.ofFloat(recognitionIndicator.progress, viewfinderSize.toFloat())
+//                    .apply {
+//                        addUpdateListener { animation ->
+//                            recognitionIndicator.progress = animation.animatedValue as Float
+//                        }
+//                        start()
+//                    }
+    }
+
+    override fun showDetails(label: Car) {
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        recognitionIndicator.visibility = View.GONE
+
+        bs_title.text = label.name
+    }
+
+    override fun frameStreamEnabled(enabled: Boolean) {
+//        if (enabled) {
+//            arSceneView.scene.setOnUpdateListener {
+//                val frame = arSceneView.arFrame ?: return@setOnUpdateListener
+//                if (frame.camera.trackingState != TrackingState.TRACKING) {
+//                    return@setOnUpdateListener
+//                }
+//                presenter.frameUpdated()
+//            }
+//            showViewFinder()
+//        } else {
+//            arSceneView.scene.setOnUpdateListener(null)
+//        }
+    }
+
+    override fun showViewFinder() {
+        recognitionIndicator.visibility = View.VISIBLE
+    }
+
+    override fun tryAttachPin() {
+//        val frame = arSceneView.arFrame ?: return
+//        val hitPoint = frame.hitTest(cameraWidth / 2f, cameraHeight / 2f).firstOrNull()
+//
+//        presenter.processHitResult(hitPoint)
+    }
+
+    companion object {
+        private const val RC_PERMISSIONS = 0x123
+    }
 }
--- app/src/main/kotlin/co/netguru/android/carrecognition/feature/camera/CameraPresenter.kt
+++ app/src/main/kotlin/co/netguru/android/carrecognition/feature/camera/CameraPresenter.kt
@@ -18,7 +18,7 @@ class CameraPresenter @Inject constructor()
     private val compositeDisposable = CompositeDisposable()
     private var processing = false
 
-    private val recognitionData = LimitedList<Recognition>(30)
+    private val recognitionData = LimitedList<Recognition>(SAMPLE_SIZE)
 
     override fun destroy() {
         super.destroy()
@@ -32,13 +32,25 @@ class CameraPresenter @Inject constructor()
             .toList()
             .sortedByDescending { it.second }
             .firstOrNull()
-        return Recognition(best?.first ?: Car.NOT_CAR, best?.second ?: 0.0)
+        return Recognition(best?.first ?: Car.NOT_CAR, best?.second?.toFloat() ?: 0.0f)
+    }
+
+    override fun bottomSheetHidden() {
+        recognitionData.clear()
+        ifViewAttached {
+            it.updateViewFinder(0f)
+            it.frameStreamEnabled(true)
+            it.showViewFinder()
+        }
     }
 
     override fun processHitResult(hitPoint: HitResult?) {
         if (hitPoint == null) {
-            //TODO: figure out what to do here (retry? cancel?)
+            ifViewAttached {
+                //TODO: figure out what to do in this case (retry? cancel?)
+            }
         } else {
+            //todo: test if point is not to close to other points in scene
             ifViewAttached {
                 it.createAnchor(hitPoint, getAverageBestRecognition().toString())
             }
@@ -70,9 +82,7 @@ class CameraPresenter @Inject constructor()
 //            .subscribeBy(
 //                onSuccess = { result ->
 //                    recognitionData.addAll(result)
-//                    ifViewAttached {
-//                        it.updateViewFinder(getViewfinderSize(getAverageBestRecognition()))
-//                    }
+//                    onFrameProcessed()
 //                },
 //                onError = { error ->
 //                    Timber.e(error)
@@ -80,10 +90,31 @@ class CameraPresenter @Inject constructor()
 //            )
     }
 
-    private fun getViewfinderSize(averageBestRecognition: Recognition): Double {
+    private fun onFrameProcessed() {
+        if (!recognitionData.isFull()) return
+
+        ifViewAttached {
+            val bestRecognition = getAverageBestRecognition()
+            val combinedResult = getViewfinderSize(getAverageBestRecognition())
+            it.updateViewFinder(combinedResult)
+
+            if (combinedResult > RECOGNITION_THRESHOLD) {
+                it.frameStreamEnabled(false)
+                it.showDetails(bestRecognition.title ?: Car.NOT_CAR)
+                it.tryAttachPin()
+            }
+        }
+    }
+
+    private fun getViewfinderSize(averageBestRecognition: Recognition): Float {
         return when (averageBestRecognition.title) {
-            Car.NOT_CAR -> 0.0
+            Car.NOT_CAR -> 0.0f
             else -> (averageBestRecognition.confidence)
         }
+    }
+
+    companion object {
+        private const val RECOGNITION_THRESHOLD = 0.30
+        private const val SAMPLE_SIZE = 30
     }
 }
