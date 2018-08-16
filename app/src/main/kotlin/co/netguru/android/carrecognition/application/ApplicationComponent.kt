e486ec482e8b99c839b3e40b86a5d81272737b05
marcin.grabowski <marcingrabowski91@gmail.com>
2018-08-16 18:19:26 +0200

    PR fix

--- app/src/main/kotlin/co/netguru/android/carrecognition/application/ApplicationComponent.kt
+++ app/src/main/kotlin/co/netguru/android/carrecognition/application/ApplicationComponent.kt
@@ -2,14 +2,12 @@ package co.netguru.android.carrecognition.application
 
 import co.netguru.android.carrecognition.application.scope.AppScope
 import dagger.Component
-import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 
 @AppScope
 @Component(
         modules = [
-            AndroidInjectionModule::class,
             AndroidSupportInjectionModule::class,
             ApplicationModule::class,
             ActivityBindingsModule::class,
