7ae2fb159ae2b025cdc5d4e8d34c2af0e274c4b5
Pawel Bochenski <pawel.bochenski@netguru.pl>
2018-10-17 11:23:02 +0200

    changed static images to show animations in onboarding

--- app/src/main/kotlin/co/netguru/android/carrecognition/feature/onboarding/OnboardingStayUpdatedFragment.kt
+++ app/src/main/kotlin/co/netguru/android/carrecognition/feature/onboarding/OnboardingStayUpdatedFragment.kt
@@ -1,25 +1,21 @@
 package co.netguru.android.carrecognition.feature.onboarding
 
+import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import co.netguru.android.carrecognition.R
 import kotlinx.android.synthetic.main.onboarding_inside_fragment.*
 
-class OnboardingStayUpdatedFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.onboarding_inside_fragment, container, false)
-    }
+class OnboardingStayUpdatedFragment : PageFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        carImg.setImageResource(R.drawable.img_onboarding_3)
         titleTxt.setText(R.string.stay_updated)
         descriptionTxt.setText(R.string.expect_updates)
     }
+
+    override fun getResourceUri(): Uri =
+        Uri.parse("android.resource://" + context?.packageName + "/" + R.raw.onboarding_3)
 
     companion object {
         val TAG = OnboardingStayUpdatedFragment::class.java.simpleName!!
