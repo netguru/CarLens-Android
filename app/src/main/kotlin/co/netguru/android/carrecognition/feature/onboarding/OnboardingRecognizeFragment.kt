f1069c1a58c4265d3634dc57e76bb76c026dd004
pbochenski <pawel.bochenski@gmail.com>
2018-10-17 14:41:10 +0200

    changed static images to show animations in onboarding (#86)
    * changed static images to show animations in onboarding
    
    * review fixes

--- app/src/main/kotlin/co/netguru/android/carrecognition/feature/onboarding/OnboardingRecognizeFragment.kt
+++ app/src/main/kotlin/co/netguru/android/carrecognition/feature/onboarding/OnboardingRecognizeFragment.kt
@@ -1,25 +1,21 @@
 package co.netguru.android.carrecognition.feature.onboarding
 
+import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import co.netguru.android.carrecognition.R
 import kotlinx.android.synthetic.main.onboarding_inside_fragment.*
 
-class OnboardingRecognizeFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.onboarding_inside_fragment, container, false)
-    }
+class OnboardingRecognizeFragment : PageFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        carImg.setImageResource(R.drawable.img_onboarding_1)
         titleTxt.setText(R.string.recognize_cars)
         descriptionTxt.setText(R.string.point_the_camera)
     }
+
+    override fun getResourceUri(): Uri =
+        Uri.parse("android.resource://${context?.packageName}/${R.raw.onboarding_1}")
 
     companion object {
         val TAG = OnboardingRecognizeFragment::class.java.simpleName!!
